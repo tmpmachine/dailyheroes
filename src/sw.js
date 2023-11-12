8e26e32817006b78a5b2458197d90c5000513ed6
tmpmachine <bill.yosafatz@gmail.com>
2023-11-12 16:26:39 +0700

    bump version

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@
   increase below number to trigger service worker update/reactivation
   to deliver latest updates for all users automatically on page visit
   
-  unique numer : 8
+  unique numer : 9
 */
 
 let cacheName = 'dailyheroes-MjQzNTM2OTU';
